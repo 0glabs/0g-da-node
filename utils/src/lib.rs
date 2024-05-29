9eca6e18383514563f718d015e24187a0ef4681a
Bruno Valente <140794260+bruno-valante@users.noreply.github.com>
2024-05-29 13:31:38 +0800

    Align implementation of map_to_g1 with golang version

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,6 +1,6 @@
-use ark_bn254::{Fq, G1Affine};
+use ark_bn254::{Fq, FqConfig, G1Affine};
 
-use ark_ff::Field;
+use ark_ff::{BigInt, Field, MontConfig, One};
 
 pub fn hex_to_bytes(s: &str) -> Option<Vec<u8>> {
     if s.len() % 2 == 0 {
@@ -17,15 +17,14 @@ pub fn hex_to_bytes(s: &str) -> Option<Vec<u8>> {
 }
 
 pub fn map_to_g1(digest: Vec<u8>) -> G1Affine {
-    let one = Fq::from(1);
-    let three = Fq::from(3);
     let mut x: Fq = num_bigint::BigUint::from_bytes_be(&digest).into();
+
     loop {
-        match (x * x * x + three).sqrt() {
+        match find_y_from_x(x) {
             Some(y) => {
                 return G1Affine::new(x, y);
             }
-            None => x += one,
+            None => x += Fq::one(),
         }
     }
 }
@@ -34,4 +33,25 @@ pub fn left_pad_zeros(x: u64, l: usize) -> Vec<u8> {
     let mut res = vec![0; l - 8];
     res.append(&mut x.to_be_bytes().to_vec());
     res
+}
+
+#[inline]
+fn find_y_from_x(x: Fq) -> Option<Fq> {
+    const SQRT_POW: BigInt<4> = match <FqConfig as MontConfig<4>>::MODULUS_PLUS_ONE_DIV_FOUR {
+        Some(x) => x,
+        None => panic!("Unsupport type"),
+    };
+
+    let beta = x * x * x + Fq::from(3);
+    // y = sqrt(beta) = beta^((p+1) / 4)
+    let y = beta.pow(SQRT_POW.as_ref());
+    (y * y == beta).then_some(y)
+}
+
+#[test]
+fn test_sqrt_consistency() {
+    for i in 1u64..200000 {
+        let x = Fq::from(i);
+        assert_eq!((x * x * x + Fq::from(3)).sqrt(), find_y_from_x(x));
+    }
 }
